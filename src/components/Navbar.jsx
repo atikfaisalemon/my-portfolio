87dcdd02c6f0572a7a28f2354adc2fd14ae82c48
atikfaisalemon <atikfaisalemon@gmail.com>
2025-02-23 04:42:35 +0600

    last-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import { div } from "framer-motion/client";
 import { useState } from "react";
 import { BiMenu, BiX } from "react-icons/bi";
 import { BsGithub, BsLinkedin, BsTwitterX } from "react-icons/bs";
