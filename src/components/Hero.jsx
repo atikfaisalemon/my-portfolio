0f6ae573cb802081198673c36287f57f86aa9156
atikfaisalemon <atikfaisalemon@gmail.com>
2025-02-22 18:30:05 +0600

    feat-hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,36 @@
-import { div } from "framer-motion/client";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <div
       id="home"
-      className="px-16 flex min-h-screen w-full items-center justify-center py-28 md:px-32"
+      className="px-16 flex min-h-screen w-full items-center justify-center py-24 md:px-32"
     >
       <div className="flex flex-col items-center justify-center gap-10 text-white">
-        <div>
+        <motion.div
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.8 }}
+        >
           <img
             src="./profile.jpg"
             alt=""
             className="w-[300px] cursor-pointer rounded-full shadow-xl shadow-indigo-950 transition-all duration-300 hover:-translate-y-5 hover:scale-105 hover:shadow-2xl hover:shadow-indigo-600 md:w-[350px]"
           />
-        </div>
+        </motion.div>
         <div className="flex max-w-[600px] flex-col items-center justify-center gap-3 text-center">
-          <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-5xl font-light transition-all duration-300 hover:opacity-100 md:text-7xl">
-            Atik Faysal Emon
+          <h1 className="bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent opacity-80 text-3xl font-light transition-all duration-300 hover:opacity-100 md:text-7xl">
+            Hi, There I'm Emon
           </h1>
+          <h3 className="bg-gradient-to-r from-pink-500  to-blue-500 bg-clip-text text-transparent opacity-80 text-3xl font-light transition-all duration-300 hover:opacity-100 md:text-5xl">
+            A frontend web developer. I write your ideas in codes
+          </h3>
+          <p className="md:text-base text-pretty text-sm">
+            Passionate about web development. I build create beautiful &
+            functional websites for small businesses. Besides coding, I own a
+            cap brand called Mad Caps. I also share my budgie breeding journey
+            on YouTube and Facebook.
+          </p>
         </div>
       </div>
     </div>
